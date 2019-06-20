bdf54ab3588677fd720f6ae2e8cc26d4995ed7e3
salmaan72 <salmaanshaik72@gmail.com>
2019-06-20 09:44:04 +0530

    verifytoken for authentication

--- src/app/leagues/routes.js
+++ src/app/leagues/routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
 import controller from './controller';
+import { verifyToken } from '../../../utils/jwtUtil';
 
 const router = express.Router();
 
-router.route('/').get(controller.getLeagues).post(controller.joinLeague);
+router.route('/').get(verifyToken, controller.getLeagues).post(verifyToken, controller.joinLeague);
 
 export { router as leaguesRoutes }
--- src/app/matches/routes.js
+++ src/app/matches/routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import controller from './controller';
+import { verifyToken } from '../../../utils/jwtUtil';
 
 const router = express.Router();
 
-router.route('/').get(controller.getMatches)
+router.route('/').get(verifyToken, controller.getMatches)
 
 export { router as matchesRoutes }
